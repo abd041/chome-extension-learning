c0599de7ed0aa74e0c6d1348b953a677e411998e
Muhammad Abdullah Rauf <abdullahrauf440@gmail.com>
2026-01-28 17:24:14 +0500

    change the background color

---
+++ content.js
@@ -0,0 +1,3 @@
+console.log("Content script loaded");
+
+document.body.style.backgroundColor = "#2381d2";
--- popup.js
+++ popup.js
@@ -1,3 +1,10 @@
-document.getElementById("btn").addEventListener("click", ()=>{
-    alert("This is my website")
-})
+document.getElementById("btn").addEventListener("click", async () => {
+  let [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
+  chrome.scripting.executeScript({
+    target: { tabId: tab.id },
+    func: () => {
+      document.body.style.backgroundColor =
+        "#" + Math.floor(Math.random() * 16777215).toString(16);
+    }
+  });
+});
