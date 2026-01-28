822bc650a20ad0b150f4c41c428334b394665a9c
Muhammad Abdullah Rauf <abdullahrauf440@gmail.com>
2026-01-28 16:34:21 +0500

    manifest version updated

--- popup.js
+++ popup.js
@@ -1,3 +1,3 @@
-document.getElementById("btn").addEventListener("click" , ()=>{
-    alert("Chrome extension is working")
+document.getElementById("btn").addEventListener("click", ()=>{
+    alert("This is my website")
 })
