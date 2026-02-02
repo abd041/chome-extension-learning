d37cf43c8ef3f1e9c0438466170cb3c07b533cc0
Muhammad Abdullah Rauf <abdullahrauf440@gmail.com>
2026-02-02 15:36:16 +0500

    count extension completed with localstorage

--- popup.js
+++ popup.js
@@ -1,9 +1,32 @@
 let count = 0;
 
-let countElement = document.getElementById("countel");
-let countBtn = document.getElementById("countbtn");
+const countElement = document.getElementById("countel");
+const countBtn = document.getElementById("countbtn");
+const minusBtn = document.getElementById("minusbtn");
+
+renderCount()
 
 countBtn.addEventListener("click", () => {
     count++;
-    countElement.innerHTML = count;
+    countElement.textContent = count;
+
+    localStorage.setItem("count" , count)
 });
+
+minusBtn.addEventListener("click", () => {
+    if (count > 0) {
+        count--;
+    }
+
+    countElement.textContent = count;
+    localStorage.setItem("count" , count)
+});
+
+
+function renderCount(){
+if(localStorage.getItem("count")){
+  count = localStorage.getItem("count")
+  countElement.textContent = count;
+
+}
+}
